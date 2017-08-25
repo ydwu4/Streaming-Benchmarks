41469917b79ef06ce407a0196e991b7abebbb671
Liu Zhi <martin823@126.com>
2017-08-25 10:48:10 +0800

    a connector which fetches records from Kafka ann then push them into Kafka again with a different topic

---
+++ streaming-benchmarks-common/src/main/java/benchmarks/common/Main.java
@@ -0,0 +1,60 @@
+package benchmarks.common;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+		
+        ParameterTool paratool = ParameterTool.fromArgs(args);
+    	//----------------------------------------------------- consumer
+		Properties props = new Properties(); 
+    	props.put("bootstrap.servers", paratool.getRequired("bootstrap.servers"));
+    	props.put("group.id", paratool.getRequired("group.id"));
+    	props.put("enable.auto.commit", "true");
+    	props.put("auto.commit.interval.ms", "1000");
+    	props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    	props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    	KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        List<String> topics = new ArrayList<>();
+        topics.add(paratool.getRequired("consumer_topic"));
+    	consumer.subscribe(topics);
+		//----------------------------------------------------- consumer		
+
+		//----------------------------------------------------- producer
+		Properties pd_props = new Properties();
+		pd_props.put("bootstrap.servers", paratool.getRequired("bootstrap.servers"));
+		pd_props.put("acks", "all");
+		pd_props.put("retries", 0);
+		pd_props.put("batch.size", 16384);
+		pd_props.put("linger.ms", 1);
+		pd_props.put("buffer.memory", 33554432);
+		pd_props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		pd_props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		String pd_topic = paratool.getRequired("producer_topic");
+		Producer<String, String> producer = new KafkaProducer<>(pd_props);
+		//----------------------------------------------------- producer
+    	
+		while (true) {
+    		ConsumerRecords<String, String> records = consumer.poll(10);
+    		int i = 1;
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.println(""+i);
+    	    	i++;
+			}
+			i = 1;
+    		for (ConsumerRecord<String, String> record : records) {
+		    	System.out.println(""+i);
+				producer.send(new ProducerRecord<String, String>(pd_topic, record.key(), record.value()));
+        		i++;
+			}
+		
+		}      
+	
+		//producer.close();
+    }
+
+}
